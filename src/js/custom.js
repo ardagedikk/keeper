49310690727ff62d4d81423aa5cdbeb1e71356ed
Arda Gedik <ardagedikk@hotmail.com>
2017-12-02 20:26:43 +0300

    Variables and options were specified

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,5 +1,42 @@
 'use strict';
 $(function(){
 
+/*
+================================================================================
+  Variables
+================================================================================
+*/
+
+// Dom Elements
+const browseInput  	= $('.browse-input'),
+			messageModal  = $('.message-modal'),
+      password   		= $('.password'),
+			passwordMeter = $('.password-strength'),
+			browseButton  = $('.browse-btn'),
+			encryptButton = $('.encrypt'),
+			decryptButton = $('.decrypt'),
+			resetButton   = $('.reset');
+
+// Temporary
+let   fileReader 			 = null,
+			encryptedFile		 = null,
+			decryptedFile 	 = null,
+			uploadedFiles    = null;
+
+// Options
+let   minPassLength = 5,
+		  maxFileSize   = 5 * 1024 * 1024; // 5MB
+
+// Enums
+const errors        = {
+
+	NO_FILES		   : 'Please select a file',
+	MAX_FILE_SIZE  : 'File size can be up to 5MB per file',
+	MIN_PASS_LENGTH: 'Password must be at least 5 characters',
+	INVALID_PASS   : 'Invalid password'
+
+}
+
+
 
 });
